6db84a0892f779a955903b7c48bb2f9d20709e20
Ivan Sen <senivan1997@gmail.com>
2026-01-28 09:35:55 +0000

    Working script

--- my_script.sh
+++ my_script.sh
@@ -9,27 +9,68 @@ Usage: ./my_script.sh [-h] [-d NUM] [-r] [PATH]
 EOF
 }
 
-isSoftLink() {
-  local filename=$1
-  local flag=0
-  for test_name in $(find -type l)
+allSoftlinks() {
+  local depth=$2
+  local start=$(realpath $1)
+  for link in $(find $start -mindepth 1 -maxdepth $depth -type l)
   do
-    local R_FILENAME=$(realpath -s $filename)
-    local R_TESTNAME=$(realpath -s $test_name)
-    if [ "$R_FILENAME" = "$R_TESTNAME" ]
-    then
-      flag=1
-      break
-    fi
+    echo File \"$link\" is a soft link
   done
-  echo $flag
 }
 
+allExec(){
+  local depth=$2
+  local start=$(realpath $1)
+  for _exec in $(find $start -mindepth 1 -maxdepth $depth -executable)
+  do 
+    echo File \"$_exec\" is an executable
+  done
+
+}
 
 
 depth=""
 recursive=0
 dir=""
 
-isSoftLink "cat.txt"
+while getopts ":hrd:" o; do
+  case "${o}" in
+    h)
+      usage
+      ;;
+    r)
+      recursive=1
+      ;;
+    d)
+      depth=${OPTARG}
+      if [[ $depth -lt 0 ]]
+      then
+	usage
+	exit 1
+      fi
+      ;;
+    *)
+      usage
+      ;;
+  esac
+done
+
+shift $((OPTIND - 1))
+dir=$@
+dir=${dir:=$(pwd)}
+find_depth=0
+if [[ $recursive -gt 0 ]]
+then
+  find_depth=$(( $depth+1 ))
+else
+  find_depth=1
+fi
+
+allExec $dir $find_depth
+allSoftlinks $dir $find_depth
+
+
+
+
+
 
